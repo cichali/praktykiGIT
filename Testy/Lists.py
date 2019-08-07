1f8f77216eff17cb54e9dc53f6a74e5a2926a174
cichali <cichyakms@gmail.com>
2019-08-07 11:42:50 +0200

    Testy

---
+++ Testy/Lists.py
@@ -0,0 +1,6 @@
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim","Jim", "Oscar", "Toby"]
+friends.sort()
+lucky_numbers.reverse()
+print(friends)
+print(lucky_numbers)
---
+++ Testy/MadLipsGame.py
@@ -0,0 +1,7 @@
+color = input("Entert a color: ")
+plural_noun = input("Entert a plural noun: ")
+celebrity = input("Entert a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
