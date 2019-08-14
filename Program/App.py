2af396cc18ba7ff783aeefd0653a7efff2d76405
cichali <cichyakms@gmail.com>
2019-08-14 19:48:58 +0200

    Program

--- Program/App.py
+++ Program/App.py
@@ -1,12 +1,16 @@
 import cv2
 import numpy
 
-def height_conv (height):
+face_cascade = cv2.CascadeClassifier('C:\\Users\\cichy\\Desktop\\Praktyki_Folder\\haarcascade_frontalface_alt.xml')
+
+# Height coordinates conversion
+def height_conv(height):
     middle_height = 240
     new_height = middle_height - height
     return new_height
 
-def width_conv (width):
+# Width coordinates conversion
+def width_conv(width):
     middle_width = 320
     new_width = middle_width + width
     return new_width
@@ -22,13 +26,19 @@ while(True):
     # Get frame size
     frame_height = numpy.size(mirror_frame, 0)
     frame_width = numpy.size(mirror_frame, 1)
-    # Draw white pixel in the middle 
-    mirror_frame[height_conv(2):height_conv(-2),width_conv(-2):width_conv(2)] = [0,0,255] 
-    test = height_conv(20)
+    # Draw red square at [0,0]
+    mirror_frame[height_conv(2):height_conv(-2),width_conv(-2):width_conv(2)] = [0, 0, 255] 
+
+    gray_frame = cv2.cvtColor(mirror_frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_frame)
+    for (x,y,w,h) in faces:
+        mirror_frame = cv2.rectangle(mirror_frame,(x,y),(x+w,y+h),(0,0,255),1)
+
     # Display the frame
     cv2.imshow('Webcam Video',mirror_frame)
+    # Exit loop on key
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        ###print("Height: " + str(frame_height) + "\nWidth: " + str(frame_width))
+        print("Height: " + str(frame_height) + "\nWidth: " + str(frame_width))
         break
 
 # Release the webcam and close window
