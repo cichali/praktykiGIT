64cbc66e8ad560612bf7036206825ac39e74cd2b
cichali <cichyakms@gmail.com>
2019-08-15 15:38:22 +0200

    Program

--- Program/App.py
+++ Program/App.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy
+import matplotlib.pyplot
+import matplotlib.animation
+from matplotlib import style
 
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
 
-frame_height = 0
-frame_width = 0
 face_cascade = cv2.CascadeClassifier('C:\\Users\\cichy\\Desktop\\Praktyki_Folder\\haarcascade_frontalface_alt.xml')
+face_center = [0, 0]
+prev_face_center = [0, 0]
 
 # Height coordinates conversion 
 def height_conv(height):
@@ -35,31 +40,34 @@ def width_conv2(width):
     return int(new_width)
 
 # Capture video from deafult webcam
+frame_height = 540
+frame_width = 960
 captured_video = cv2.VideoCapture(0)
-captured_video.set(3, 960)
-captured_video.set(4, 540)
+captured_video.set(3, frame_width)
+captured_video.set(4, frame_height)
 
 while(True):
     # Obtain a frame from video and flip
     ret, frame = captured_video.read()
-    mirror_frame = cv2.flip(frame, 1)
-    # Get frame size
-    frame_height = numpy.size(mirror_frame, 0)
-    frame_width = numpy.size(mirror_frame, 1)
-    # Draw circle at [0, 0]
-    mirror_frame = cv2.circle(mirror_frame, (width_conv(0), height_conv(0)), 3, (0,0,255), 1)
+    frame = cv2.flip(frame, 1)
+    # Draw a circle at [0, 0]
+    frame = cv2.circle(frame, (width_conv(0), height_conv(0)), 3, (255, 255, 255), 1)
     # Convert frame to grey
-    gray_frame = cv2.cvtColor(mirror_frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Face detection
     faces = face_cascade.detectMultiScale(gray_frame)
     # Draw rectangles around faces, circle in the middle, print coordinates
     for (x,y,w,h) in faces:
-        mirror_frame = cv2.rectangle(mirror_frame, (x,y), (x+w,y+h), (0,0,255), 1)
-        face_middle = [height_conv2(y+(h/2)), width_conv2(x+(w/2))]
-        print("Height: " + str(face_middle[0]) + " Width: " + str(face_middle[1]))
-        mirror_frame = cv2.circle(mirror_frame, (int(x+(w/2)), int(y+(h/2))), 3, (0,255,0), 1)
-    # Display the frame
-    cv2.imshow('Webcam Video', mirror_frame)
+        frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 1)
+        face_center = [height_conv2(y+(h/2)), width_conv2(x+(w/2))]
+        frame = cv2.circle(frame, (int(x+(w/2)), int(y+(h/2))), 3, (255,0,0), 1)
+
+    # Display the actual frame
+    print("Height: " + str(face_center[0]) + " Width: " + str(face_center[1]))
+    frame = cv2.line(frame, (width_conv(face_center[1]), height_conv(face_center[0])), (width_conv(prev_face_center[1]), height_conv(prev_face_center[0])), (0, 255, 0), 1)
+
+    cv2.imshow('Webcam Video', frame)
+    prev_face_center = face_center
     # Exit loop on key
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print("Height: " + str(frame_height) + "\nWidth: " + str(frame_width))
